3925b2007872ae7a78147313ff60de0731cb05c4
Staffan Olsson <staffan@repos.se>
2015-06-01 14:33:47 +0200

    Renders model changes but fails to see add

--- backbone-polymer-mixin.js
+++ backbone-polymer-mixin.js
@@ -1,7 +1,25 @@
 
+// TODO both polymerNotify and the global name are misleading for a setup/initialize type of function
+
 
 var BackbonePolymerNotify = function(element, pathPrefix) {
   console.log('BackbonePolymerNotify called', this, element, pathPrefix);
+
+  var indexOf = this.indexOf.bind(this);
+
+  var modelSetup = function(model) {
+    this.listenTo(model, 'change', function(ev) {
+      for (var attribute in ev.changed) {
+        console.log('model', model.cid, 'changed', attribute);
+        var ix = indexOf(model);
+        var value = model.get(attribute);
+        element.notifyPath(pathPrefix + '.models.' + ix + '.attributes.' + attribute, value);
+      }
+    });
+  };
+
+  this.each(modelSetup.bind(this));
+  this.on('add', modelSetup.bind(this));
 };
 
 if (typeof module !== 'undefined') {
