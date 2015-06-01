61454a59c28f1852b313ef433cad3b9c5aacb252
Staffan Olsson <staffan@repos.se>
2015-06-01 13:17:01 +0200

    Adds tests for lib's existence in a regular page with script tags for dependencies and lib

---
+++ backbone-polymer-mixin.js
@@ -0,0 +1,11 @@
+
+
+var BackbonePolymerNotify = function(element, pathPrefix) {
+  console.log('BackbonePolymerNotify called', element, pathPrefix);
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    polymerNotify: BackbonePolymerNotify
+  };
+};
